c77a2ebeefe0a7a6ebdb2917ee7ce01af750100c
Volodymyr Moiseichenko <alternativeqoob@gmail.com>
2022-12-28 20:03:44 +0200

    Working on host

--- Program.cs
+++ Program.cs
@@ -19,34 +19,9 @@ namespace Notes.Identity
                 .AddInMemoryClients(Configuration.Clients)
                 .AddDeveloperSigningCredential();
 
-            var connectionString = builder.Services.AddDbContext<DbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
             
 
-
-            var host = Host.CreateDefaultBuilder(args).Build();
-            using(var scope = host.Services.CreateScope())
-            {
-                var serviceProvider = scope.ServiceProvider;
-                try
-                {
-                    var context = serviceProvider.GetRequiredService<AuthDbContext>;
-                    DbInitializer.Initialize(context);
-                }
-                catch(Exception ex)
-                {
-                    var logger = serviceProvider.GetRequiredService<ILogger<Program>>;
-                    logger.LogError(ex, "An error occured while app initialization");
-                }
-            }
-            host.Run();
-
-
-            builder.Services.AddDbContext<AuthDbContext>(options =>
-            {
-                options.UseSqlServer(connectionString);
-            });
-                   
+   
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
