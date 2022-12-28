987eb9dc03088aa3ba36624509744455384b016a
Volodymyr Moiseichenko <alternativeqoob@gmail.com>
2022-12-28 22:23:52 +0200

    Finished configuration for IdentityServer

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Notes.Identity.Data;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
+using Notes.Identity.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Notes.Identity
 {
@@ -12,20 +14,55 @@ namespace Notes.Identity
 
             var builder = WebApplication.CreateBuilder(args);
 
+            var connectionString = builder.Configuration.GetConnectionString("DbConnextion");
+            builder.Services.AddDbContext<AuthDbContext>(x => x.UseSqlServer(connectionString));
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+            {
+                config.Password.RequiredLength = 12;
+                config.Password.RequireDigit = false;
+                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireUppercase = true;
+                config.Password.RequireLowercase = true;
+            })
+                .AddEntityFrameworkStores<AuthDbContext>()
+                .AddDefaultTokenProviders();
+
+
             builder.Services.AddIdentityServer()
+                .AddAspNetIdentity<AppUser>()
                 .AddInMemoryApiResources(Configuration.ApiResource)
                 .AddInMemoryIdentityResources(Configuration.IdentityResources)
                 .AddInMemoryApiScopes(Configuration.ApiScopes)
                 .AddInMemoryClients(Configuration.Clients)
                 .AddDeveloperSigningCredential();
 
-            
+            builder.Services.ConfigureApplicationCookie(config =>
+            {
+                config.Cookie.Name = "Notes.Identity.Cookie";
+                config.LoginPath = "/Auth/Login";
+                config.LogoutPath = "/Auth/Logout";
+            });
 
    
             var app = builder.Build();
 
             app.MapGet("/", () => "Hello World!");
-
+            using(var scope = app.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                try
+                {
+                    var context = serviceProvider.GetRequiredService<AuthDbContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while app initialization");
+                }
+            }
+           
             app.UseRouting();
             app.UseIdentityServer();
             app.Run();
